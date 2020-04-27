6ef5d504ce5172471ffcfc2c4c1d699c2f2656ff
Pavel Korn <lyavolpe@gmail.com>
2020-04-27 22:21:26 +0300

    Tower shop item
    Сделал заготовку для покупки и сноса башен

---
+++ Assets/Scripts/Objects/TowerShopItem.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Controllers;
+
+namespace Objects
+{
+    public class TowerShopItem : MonoBehaviour
+    {
+        public GameProxy Controller;
+        public TowrerWeapon Weapon;
+        public int BuildCost;
+        public int DestroyCost;
+        private void OnEnable()
+        {
+            OnBuilt();
+        }
+
+        private void OnDisable()
+        {
+            OnDestroyed();
+        }
+
+        private void OnBuilt()
+        {
+            Controller.SubtractCoins(BuildCost);
+        }
+
+        private void OnDestroyed()
+        {
+            Controller.AddCoins(DestroyCost);
+        }
+    }
+}
+
